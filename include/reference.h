47dafa8032c5a0974e2010ffd757f866b6869e08
zeno40 <dhaene.filip@skynet.be>
2017-10-16 01:13:26 +0200

    latest build

--- include/reference.h
+++ include/reference.h
@@ -43,58 +43,44 @@ void nnp_convolution_kernel_gradient__reference(
 	const float* grad_output,
 	float* grad_kernel);
 
-//void nnp_fully_connected_output_f32__reference(
-//	size_t batch_size,
-//	size_t input_channels,
-//	size_t output_channels,
-//	const float* input,
-//	const float* kernel,
-//	float* output,
-//	pthreadpool_t threadpool);
-//
-//void nnp_fully_connected_output_f16f32__reference(
-//	size_t batch_size,
-//	size_t input_channels,
-//	size_t output_channels,
-//	const float* input,
-//	const void* kernel,
-//	float* output,
-//	pthreadpool_t threadpool);
-//
-//void nnp_max_pooling_output__reference(
-//	size_t batch_size,
-//	size_t channels,
-//	struct nnp_size input_size,
-//	struct nnp_padding input_padding,
-//	struct nnp_size pooling_size,
-//	struct nnp_size pooling_stride,
-//	const float input[],
-//	float output[],
-//	pthreadpool_t threadpool);
-//
-//void nnp_relu_output__reference(
-//	size_t batch_size,
-//	size_t channels,
-//	const float input[],
-//	float output[],
-//	float negative_slope,
-//	pthreadpool_t threadpool);
-//
-//void nnp_relu_input_gradient__reference(
-//	size_t batch_size,
-//	size_t channels,
-//	const float grad_output[],
-//	const float input[],
-//	float grad_input[],
-//	float negative_slope,
-//	pthreadpool_t threadpool);
-//
-//void nnp_softmax_output__reference(
-//    size_t batch_size,
-//    size_t channels,
-//    const float input[],
-//    float output[],
-//    pthreadpool_t threadpool);
+void nnp_fully_connected_output_f32__reference(
+	const size_t batch_size,
+	const size_t input_channels,
+	const size_t output_channels,
+	const float* input,
+	const float* kernel,
+	float* output);
+
+void nnp_max_pooling_output__reference(
+	const size_t batch_size,
+	const size_t channels,
+	const struct nnp_size input_size,
+	const struct nnp_padding input_padding,
+	const struct nnp_size pooling_size,
+	const struct nnp_size pooling_stride,
+	const float* input,
+	float* output);
+
+void nnp_relu_output__reference(
+	const size_t batch_size,
+	const size_t channels,
+	const float* input,
+	float* output,
+	const float negative_slope);
+
+void nnp_relu_input_gradient__reference(
+	const size_t batch_size,
+	const size_t channels,
+	const float* grad_output,
+	const float* input,
+	float* grad_input,
+	const float negative_slope);
+
+void nnp_softmax_output__reference(
+	const size_t batch_size,
+	const size_t channels,
+	const float* input,
+	float* output);
 
 #ifdef __cplusplus
 } /* extern "C" */
