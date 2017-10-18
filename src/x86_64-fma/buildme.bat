38e0165ce40f351eadc47c18a20b9f39286398b6
zeno40 <dhaene.filip@skynet.be>
2017-10-18 02:10:40 +0200

    latest build

---
+++ src/x86_64-fma/buildme.bat
@@ -0,0 +1,27 @@
+set current_dir=%cd%
+
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\2d-fourier-8x8.obj %current_dir%\2d-fourier-8x8.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\2d-fourier-16x16.obj %current_dir%\2d-fourier-16x16.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\2d-winograd-8x8-3x3.obj %current_dir%\2d-winograd-8x8-3x3.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\max-pooling.obj %current_dir%\max-pooling.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\relu.obj %current_dir%\relu.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\softmax.obj %current_dir%\softmax.py
+
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\blas\s8gemm.obj %current_dir%\blas\s8gemm.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\blas\c8gemm.obj %current_dir%\blas\c8gemm.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\blas\s4c6gemm.obj %current_dir%\blas\s4c6gemm.py
+
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\blas\conv1x1.obj %current_dir%\blas\conv1x1.py
+
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\blas\sgemm.obj %current_dir%\blas\sgemm.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\blas\sdotxf.obj %current_dir%\blas\sdotxf.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\blas\shdotxf.obj %current_dir%\blas\shdotxf.py
+
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\fft-soa.obj %current_dir%\fft-soa.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\fft-aos.obj %current_dir%\fft-aos.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\fft-dualreal.obj %current_dir%\fft-dualreal.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\ifft-dualreal.obj %current_dir%\ifft-dualreal.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\fft-real.obj %current_dir%\fft-real.py
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\ifft-real.obj %current_dir%\ifft-real.py
+
+python -m peachpy.x86_64 -mabi=ms -mimage-format=ms-coff -mcpu=haswell -o %current_dir%\winograd-f6k3.obj %current_dir%\winograd-f6k3.py
