2bdd3bbcda5e35dab0d24bf60303d2e9c03b043f
filip dhaene <dhaene.filip@skynet.be>
2017-10-23 22:25:48 +0200

    latest build

--- test/convolution-inference/overfeat-fast.cc
+++ test/convolution-inference/overfeat-fast.cc
@@ -101,18 +101,6 @@ TEST(WT8x8, conv3_with_relu) {
 		.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
 }
 
-TEST(WT8x8_FP16, conv3) {
-	OverFeat_Fast::conv3()
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-}
-
-TEST(WT8x8_FP16, conv3_with_relu) {
-	OverFeat_Fast::conv3()
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
-}
-
 TEST(IMPLICIT_GEMM, conv3) {
 	OverFeat_Fast::conv3()
 		.errorLimit(1.0e-5)
@@ -163,18 +151,6 @@ TEST(WT8x8, conv4_with_relu) {
 	OverFeat_Fast::conv4()
 		.errorLimit(1.0e-5)
 		.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
-}
-
-TEST(WT8x8_FP16, conv4) {
-	OverFeat_Fast::conv4()
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-}
-
-TEST(WT8x8_FP16, conv4_with_relu) {
-	OverFeat_Fast::conv4()
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
 }
 
 TEST(IMPLICIT_GEMM, conv4) {
@@ -229,18 +205,6 @@ TEST(WT8x8, conv5_with_relu) {
 		.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
 }
 
-TEST(WT8x8_FP16, conv5) {
-	OverFeat_Fast::conv5()
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-}
-
-TEST(WT8x8_FP16, conv5_with_relu) {
-	OverFeat_Fast::conv5()
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
-}
-
 TEST(IMPLICIT_GEMM, conv5) {
 	OverFeat_Fast::conv5()
 		.errorLimit(1.0e-5)
@@ -256,7 +220,7 @@ TEST(IMPLICIT_GEMM, conv5_with_relu) {
 int main(int argc, char* argv[]) {
 	const enum nnp_status init_status = nnp_initialize();
 	assert(init_status == nnp_status_success);
-	setenv("TERM", "xterm-256color", 0);
+	//setenv("TERM", "xterm-256color", 0);
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
--- test/convolution-inference/smoke.cc
+++ test/convolution-inference/smoke.cc
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <nnpack.h>
-#include <nnpack/hwinfo.h>
+#include <hwinfo.h>
 
 #include <testers/convolution.h>
 
@@ -57,22 +57,6 @@ TEST(WT8x8, single_tile_with_relu) {
 		.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
 }
 
-TEST(WT8x8_FP16, single_tile) {
-	ConvolutionTester()
-		.inputSize(8, 8)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-}
-
-TEST(WT8x8_FP16, single_tile_with_relu) {
-	ConvolutionTester()
-		.inputSize(8, 8)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
-}
-
 /*
  * Test that the implementation handles extraction of input subtile
  */
@@ -125,22 +109,6 @@ TEST(WT8x8, input_subtile_with_relu) {
 		.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
 }
 
-TEST(WT8x8_FP16, input_subtile) {
-	ConvolutionTester()
-		.inputSize(4, 4)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-}
-
-TEST(WT8x8_FP16, input_subtile_with_relu) {
-	ConvolutionTester()
-		.inputSize(4, 4)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
-}
-
 /*
  * Test that the implementation handles multi-tile inputs
  */
@@ -191,22 +159,6 @@ TEST(WT8x8, multi_tile_with_relu) {
 		.iterations(100)
 		.errorLimit(1.0e-3)
 		.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
-}
-
-TEST(WT8x8_FP16, multi_tile) {
-	ConvolutionTester()
-		.inputSize(13, 13)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-}
-
-TEST(WT8x8_FP16, multi_tile_with_relu) {
-	ConvolutionTester()
-		.inputSize(13, 13)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
 }
 
 /*
@@ -321,42 +273,6 @@ TEST(WT8x8, implicit_padding_with_relu) {
 	}
 }
 
-TEST(WT8x8_FP16, implicit_padding) {
-	ConvolutionTester tester;
-	tester.inputSize(8, 8)
-		.kernelSize(3, 3)
-		.iterations(15)
-		.errorLimit(1.0e-1);
-	for (size_t paddingTop = 0; paddingTop < tester.kernelHeight(); paddingTop++) {
-		for (size_t paddingRight = 0; paddingRight < tester.kernelWidth(); paddingRight++) {
-			for (size_t paddingLeft = 0; paddingLeft < tester.kernelWidth(); paddingLeft++) {
-				for (size_t paddingBottom = 0; paddingBottom < tester.kernelHeight(); paddingBottom++) {
-					tester.inputPadding(paddingTop, paddingRight, paddingBottom, paddingLeft)
-						.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-				}
-			}
-		}
-	}
-}
-
-TEST(WT8x8_FP16, implicit_padding_with_relu) {
-	ConvolutionTester tester;
-	tester.inputSize(8, 8)
-		.kernelSize(3, 3)
-		.iterations(15)
-		.errorLimit(1.0e-1);
-	for (size_t paddingTop = 0; paddingTop < tester.kernelHeight(); paddingTop++) {
-		for (size_t paddingRight = 0; paddingRight < tester.kernelWidth(); paddingRight++) {
-			for (size_t paddingLeft = 0; paddingLeft < tester.kernelWidth(); paddingLeft++) {
-				for (size_t paddingBottom = 0; paddingBottom < tester.kernelHeight(); paddingBottom++) {
-					tester.inputPadding(paddingTop, paddingRight, paddingBottom, paddingLeft)
-						.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
-				}
-			}
-		}
-	}
-}
-
 /*
  * Test that the implementation can handle small non-unit number of input channels
  */
@@ -427,28 +343,6 @@ TEST(WT8x8, few_input_channels_with_relu) {
 	}
 }
 
-TEST(WT8x8_FP16, few_input_channels) {
-	ConvolutionTester tester;
-	tester.inputSize(8, 8)
-		.iterations(100)
-		.errorLimit(1.0e-2);
-	for (size_t inputChannels = 2; inputChannels <= 5; inputChannels++) {
-		tester.inputChannels(inputChannels)
-			.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-	}
-}
-
-TEST(WT8x8_FP16, few_input_channels_with_relu) {
-	ConvolutionTester tester;
-	tester.inputSize(8, 8)
-		.iterations(100)
-		.errorLimit(1.0e-2);
-	for (size_t inputChannels = 2; inputChannels <= 5; inputChannels++) {
-		tester.inputChannels(inputChannels)
-			.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
-	}
-}
-
 /*
  * Test that the implementation can handle small non-unit number of output channels
  */
@@ -516,28 +410,6 @@ TEST(WT8x8, few_output_channels_with_relu) {
 	for (size_t outputChannels = 2; outputChannels <= 5; outputChannels++) {
 		tester.outputChannels(outputChannels)
 			.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
-	}
-}
-
-TEST(WT8x8_FP16, few_output_channels) {
-	ConvolutionTester tester;
-	tester.inputSize(8, 8)
-		.iterations(100)
-		.errorLimit(3.0e-2);
-	for (size_t outputChannels = 2; outputChannels <= 5; outputChannels++) {
-		tester.outputChannels(outputChannels)
-			.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-	}
-}
-
-TEST(WT8x8_FP16, few_output_channels_with_relu) {
-	ConvolutionTester tester;
-	tester.inputSize(8, 8)
-		.iterations(100)
-		.errorLimit(3.0e-2);
-	for (size_t outputChannels = 2; outputChannels <= 5; outputChannels++) {
-		tester.outputChannels(outputChannels)
-			.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
 	}
 }
 
@@ -631,22 +503,6 @@ TEST(WT8x8, non_square_image_with_relu) {
 		.iterations(100)
 		.errorLimit(1.0e-3)
 		.testInference(nnp_convolution_algorithm_wt8x8, nnp_activation_relu);
-}
-
-TEST(WT8x8_FP16, non_square_image) {
-	ConvolutionTester tester;
-	tester.inputSize(9, 10)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16);
-}
-
-TEST(WT8x8_FP16, non_square_image_with_relu) {
-	ConvolutionTester tester;
-	tester.inputSize(9, 10)
-		.iterations(100)
-		.errorLimit(1.0e-2)
-		.testInference(nnp_convolution_algorithm_wt8x8_fp16, nnp_activation_relu);
 }
 
 /*
@@ -826,7 +682,7 @@ TEST(DIRECT_1x1, odd_image_size_with_relu) {
 int main(int argc, char* argv[]) {
 	const enum nnp_status init_status = nnp_initialize();
 	assert(init_status == nnp_status_success);
-	setenv("TERM", "xterm-256color", 0);
+	//setenv("TERM", "xterm-256color", 0);
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
