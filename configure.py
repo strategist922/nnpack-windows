24fb7461ce527f858ba18bcd818441deb7c376ef
zeno40 <zeno40@users.noreply.github.com>
2017-12-01 16:20:28 +0100

    Update configure.py

--- configure.py
+++ configure.py
@@ -54,7 +54,7 @@ def main(args):
         nnpack_objects = [
             build.cc("init.c"),
             build.cc("convolution-inference.c"),
-            build.cc("pthreadpool.c")
+            build.cxx("pthreadpool.cpp")
         ]
         if not options.convolution_only:
             # Fully-connected, pooling, Softmax, ReLU layers
