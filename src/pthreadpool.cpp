85f2a01cc2e9573445cbf6123cc3e5adc6ba22c7
filip dhaene <dhaene.filip@skynet.be>
2017-12-01 16:43:28 +0100

    latest build

--- src/pthreadpool.cpp
+++ src/pthreadpool.cpp
@@ -1,14 +1,84 @@
-#if defined(_MSC_VER) && defined(__cplusplus)
-#include <ppl.h>
-#endif
+#if defined(_MSC_VER)
+	#include <ppl.h>
+#else
+	#include <cassert>
+	#include <cstdio>
+	#include <limits>
+	#include <string>
+	#include <type_traits>
+	#include <utility>
+	#include <vector>
 
+	struct blocked_range {
+		typedef size_t const_iterator;
+
+		blocked_range(size_t begin, size_t end) : begin_(begin), end_(end) {}
+		blocked_range(int begin, int end) : begin_(begin), end_(end) {}
+
+		const_iterator begin() const { return begin_; }
+		const_iterator end() const { return end_; }
+
+	private:
+		size_t begin_;
+		size_t end_;
+	};
+
+	template <typename Func>
+	void xparallel_for(size_t begin, size_t end, const Func &f) {
+		blocked_range r(begin, end);
+		f(r);
+	}
+
+	template <typename Func>
+	void parallel_for(size_t begin,
+		size_t end,
+		const Func &f,
+		size_t /*grainsize*/) {
+		assert(end >= begin);
+		// unsigned index isn't allowed in OpenMP 2.0
+	#pragma omp parallel for
+		for (int i = static_cast<int>(begin); i < static_cast<int>(end); ++i)
+			f(blocked_range(i, i + 1));
+	}
+	template <typename T, typename U>
+	bool value_representation(U const &value) {
+		return static_cast<U>(static_cast<T>(value)) == value;
+	}
+
+	template <typename T, typename Func>
+	inline void for_(
+		bool parallelize, size_t begin, T end, Func f, size_t grainsize = 100) {
+		static_assert(std::is_integral<T>::value, "end must be integral type");
+		parallelize = parallelize && value_representation<size_t>(end);
+		parallelize ? parallel_for(begin, end, f, grainsize)
+			: xparallel_for(begin, end, f);
+	}
+
+	template <typename T, typename Func>
+	inline void for_i(bool parallelize, T size, Func f, size_t grainsize = 100u) {
+		for_(parallelize, 0u, size,
+			[&](const blocked_range &r) {
+
+			for (int i = static_cast<int>(r.begin());
+				i < static_cast<int>(r.end()); i++) {
+				f(i);
+			}
+
+		}, grainsize);
+	}
+
+	template <typename T, typename Func>
+	inline void for_i(T size, Func f, size_t grainsize = 100) {
+		for_i(true, size, f, grainsize);
+	}
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	#include <nnpack/pthreadpool.h>
-	#include <nnpack/utils.h>
+#include <nnpack/pthreadpool.h>
+#include <nnpack/utils.h>
 
 
 	void pthreadpool_compute_1d(
@@ -22,10 +92,9 @@ extern "C" {
 			function(argument, i);
 		}, concurrency::static_partitioner());
 #else
-		long long i;
-#pragma omp parallel for schedule(static)
-		for (i = 0; i < range; i++)
+		for_i(range, [&](size_t i) {
 			function(argument, i);
+		});
 #endif
 	}
 
